7cad77282eaa6d963a2985b8a2b66f9ebf90c88d
Andrew Johnston <asjohnston@alaska.edu>
2024-10-14 14:06:20 -0800

    first draft

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # s2-staging
-Small app for copying Sentinel-2 data from Google Cloud Storage to AWS S3
+Small app for copying Sentinel-2 B08.jp2 files from Google Cloud Storage to AWS S3
+
+For example, for scene `S2A_MSIL1C_20231213T235751_N0510_R087_T51CWT_20231214T003659`:
+- https://storage.googleapis.com/gcp-public-data-sentinel-2/tiles/51/C/WT/S2A_MSIL1C_20231213T235751_N0510_R087_T51CWT_20231214T003659.SAFE/./GRANULE/L1C_T51CWT_A044271_20231213T235749/IMG_DATA/T51CWT_20231213T235751_B08.jp2
+
+is copied to:
+- `s3://myBucket/myPrefix/S2A_MSIL1C_20231213T235751_N0510_R087_T51CWT_20231214T003659_B08.jp2`
+
+## Local Usage
+
+Requires AWS credentials with `s3:PutObject` permissions for `arn:aws:s3:::myBucket/myPrefix/*`.
+
+```commandline
+git clone git@github.com:ASFHyP3/s2-staging.git
+cd s2-staging
+mamba env create -f environment.yml
+mamba activate s2-staging
+python
+```
+
+```python
+from main import fetch_scene
+fetch_scene('S2B_MSIL1C_20240216T235509_N0510_R087_T51CWT_20240217T005730', 'myBucket', 'myPrefix/')
+```
---
+++ src/main.py
@@ -0,0 +1,70 @@
+import json
+import logging
+import os
+import xml.etree.ElementTree as ET
+
+import boto3
+import requests
+
+log = logging.getLogger('its_live_monitoring')
+log.setLevel(logging.INFO)
+
+s3 = boto3.client('s3')
+session = requests.Session()
+
+
+def get_s2_safe_url(scene_name):
+    root_url = 'https://storage.googleapis.com/gcp-public-data-sentinel-2/tiles'
+    tile = f'{scene_name[39:41]}/{scene_name[41:42]}/{scene_name[42:44]}'
+    return f'{root_url}/{tile}/{scene_name}.SAFE'
+
+
+def get_s2_manifest(safe_url: str):
+    manifest_url = f'{safe_url}/manifest.safe'
+    response = requests.get(manifest_url)
+    response.raise_for_status()
+    return response.text
+
+
+def get_s2_path(scene_name: str) -> str:
+    safe_url = get_s2_safe_url(scene_name)
+
+    manifest_text = get_s2_manifest(safe_url)
+    root = ET.fromstring(manifest_text)
+    elements = root.findall(".//fileLocation[@locatorType='URL'][@href]")
+    hrefs = [element.attrib['href'] for element in elements if
+             element.attrib['href'].endswith('_B08.jp2') and '/IMG_DATA/' in element.attrib['href']]
+    if len(hrefs) == 1:
+        # post-2016-12-06 scene; only one tile
+        file_path = hrefs[0]
+    else:
+        # pre-2016-12-06 scene; choose the requested tile
+        tile_token = scene_name.split('_')[5]
+        file_path = [href for href in hrefs if href.endswith(f'_{tile_token}_B08.jp2')][0]
+
+    return f'{safe_url}/{file_path}'
+
+
+def fetch_scene(scene_name, bucket, bucket_prefix):
+    google_cloud_url = get_s2_path(scene_name)
+    response = session.get(google_cloud_url, stream=True)
+    response.raise_for_status()
+
+    key = f'{bucket_prefix}{scene_name}_B08.jp2'
+    s3.upload_fileobj(response.raw, bucket, key)
+
+
+def lambda_handler(event: dict, context: object) -> dict:
+    bucket = os.getenv('BUCKET')
+    bucket_prefix = os.getenv('BUCKET_PREFIX')
+
+    batch_item_failures = []
+    for record in event['Records']:
+        try:
+            body = json.loads(record['body'])
+            scene = body['Message']
+            fetch_scene(scene, bucket, bucket_prefix)
+        except Exception:
+            log.exception(f'Could not process message {record["messageId"]}')
+            batch_item_failures.append({'itemIdentifier': record['messageId']})
+    return {'batchItemFailures': batch_item_failures}
